a34447a9cc9c6439bbbf9fd28f7d7781eecf6a70
Guillaume Houis <guillaume.houis@gmail.com>
2022-12-25 11:33:29 +0000

    type request

--- src/controllers/test/test.controller.ts
+++ src/controllers/test/test.controller.ts
@@ -1,19 +1,20 @@
-import express, { Request, Response } from 'express';
+import express, { Response } from 'express';
 import { validate } from '../../lib/validator/validate.middleware';
-import { postTestSchema } from './test.validations';
+import { ArticleDto, postTestSchema } from './test.validations';
 import { ProjectError, WsErrors } from '../../lib/error/error.types';
+import { TypedRequest } from '../../lib/request/TypedRequest.type';
 
 const testController = express.Router();
 
-testController.get('/', (req: Request, res: Response) => {
+testController.get('/', (req: TypedRequest<undefined>, res: Response) => {
   res.status(204).end();
 });
 
-testController.get('/error', (req: Request, res: Response) => {
+testController.get('/error', () => {
   throw new ProjectError(WsErrors.ERROR);
 });
 
-testController.post('/', validate(postTestSchema), (req: Request, res: Response) => {
+testController.post('/', validate(postTestSchema), (req: TypedRequest<ArticleDto>, res: Response) => {
   res.status(200).send(req.body);
 });
 
--- src/lib/validator/validate.middleware.ts
+++ src/lib/validator/validate.middleware.ts
@@ -14,21 +14,23 @@ type RequestData = {
     query?: unknown
 }
 
-export const validate = (schema: RequestSchema) => (req: Request, res: Response, next: NextFunction): void => {
-    const toValidate: RequestData = {};
-    if (req?.params && Object.keys(req?.params).length ) toValidate.params = req.params;
-    if (req?.query && Object.keys(req?.query).length) toValidate.query = req.query;
-    if (req?.body && Object.keys(req?.body).length) toValidate.body = req.body;
+export function validate (schema: RequestSchema) {
+ return (req: Request, res: Response, next: NextFunction): void => {
+     const toValidate: RequestData = {};
+     if (req?.params && Object.keys(req?.params).length ) toValidate.params = req.params;
+     if (req?.query && Object.keys(req?.query).length) toValidate.query = req.query;
+     if (req?.body && Object.keys(req?.body).length) toValidate.body = req.body;
 
-    const { value, error } = Joi.compile(schema)
-        .prefs({abortEarly: false})
-        .validate(toValidate);
+     const { value, error } = Joi.compile(schema)
+       .prefs({abortEarly: false})
+       .validate(toValidate);
 
-    if (error) {
-        const errors: FieldError[] = error.details.map((details: ValidationErrorItem) => ({field: details.context.key, reason: details.message}));
-        return next(new BadRequestError(WsErrors.badRequest.default, errors));
-    }
+     if (error) {
+         const errors: FieldError[] = error.details.map((details: ValidationErrorItem) => ({field: details.context.key, reason: details.message}));
+         return next(new BadRequestError(WsErrors.badRequest.default, errors));
+     }
 
-    Object.assign(req, value);
-    return next();
-};
+     Object.assign(req, value);
+     return next();
+ };   
+}
